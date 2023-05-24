dd5f512f4f7d76e5b2792b18e99721c3e1c31294
Roy Suthers <rsuthers@cogeco.ca>
2023-05-24 08:18:54 -0400

    Moved Flask code from get_player_data.py to main.py

--- python/get_player_data.py
+++ python/get_player_data.py
@@ -3042,31 +3042,3 @@ def stats_config(position: str='all') -> Tuple[List, List, List, Dict, List]:
     config['columns'].extend(goalie_p60_zscore_columns['columns'])
 
     return deepcopy(config)
-
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-
-app = Flask(__name__)
-CORS(app)
-
-@app.route('/player-data')
-def player_data():
-
-    # Get the values for the request parameters
-    season_or_date_radios = request.args.get('seasonOrDateRadios')
-    from_season = request.args.get('fromSeason')
-    to_season = request.args.get('toSeason')
-    from_date = request.args.get('fromDate')
-    to_date = request.args.get('toDate')
-    game_type = request.args.get('gameType')
-    stat_type = request.args.get('statType')
-    pool_id = request.args.get('poolID')
-
-    # Call your get_player_data function with the specified parameters
-    data_dict = rank_players(season_or_date_radios, from_season, to_season, from_date, to_date, pool_id, game_type, stat_type)
-
-    # Return the player data as JSON
-    return jsonify(data_dict).get_json()
-
-if __name__ == '__main__':
-    app.run()
--- python/main.py
+++ python/main.py
@@ -1,3 +1,29 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
 from get_player_data import rank_players
 
-json_data = rank_players(from_season_id='20222023', to_season_id='20222023', season_type='P')
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/player-data')
+def player_data():
+
+    # Get the values for the request parameters
+    season_or_date_radios = request.args.get('seasonOrDateRadios')
+    from_season = request.args.get('fromSeason')
+    to_season = request.args.get('toSeason')
+    from_date = request.args.get('fromDate')
+    to_date = request.args.get('toDate')
+    game_type = request.args.get('gameType')
+    stat_type = request.args.get('statType')
+    pool_id = request.args.get('poolID')
+
+    # Call your get_player_data function with the specified parameters
+    data_dict = rank_players(season_or_date_radios, from_season, to_season, from_date, to_date, pool_id, game_type, stat_type)
+
+    # Return the player data as JSON
+    return jsonify(data_dict).get_json()
+
+if __name__ == '__main__':
+    app.run()
