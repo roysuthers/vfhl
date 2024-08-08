c2c9ce3c42383d270654b6160bc9bd23a9c2be5b
Roy Suthers <rsuthers@cogeco.ca>
2024-08-08 08:41:48 -0400

    Append '\\python' to geckodriver.exe path

--- python/clsBrowser.py
+++ python/clsBrowser.py
@@ -61,7 +61,7 @@ class Browser:
                 options.set_preference("browser.download.dir", self.browser_download_dir)
                 options.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/octet-stream")
 
-            driver_path  = Path(os.getcwd()) / DRIVER_NAME
+            driver_path  = Path(os.getcwd() + '\\python') / DRIVER_NAME
             self.browser = Firefox(executable_path=str(driver_path) , options=options)
 
             self.browser.set_page_load_timeout(30)  # Set timeout to 10 seconds
